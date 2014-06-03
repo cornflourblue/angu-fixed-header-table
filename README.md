87cbecf6aad4762d12208c982fba89a3a29395b0
Jason Watmore <cornflourblue@hotmail.com>
2014-06-03 12:08:29 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 angu-fixed-header-table
 =======================
 
-AngularJS fixed header scrollable table directive
+An AngularJS fixed header scrollable table directive
---
+++ angu-fixed-header-table.js
@@ -0,0 +1,57 @@
+﻿/**
+ * AngularJS fixed header scrollable table directive
+ * @author Jason Watmore <jason@pointblankdevelopment.com.au> (http://jasonwatmore.com)
+ * @version 1.0.0
+ */
+(function () {
+    angular.module('anguFixedHeaderTable', [])
+    .directive('fixedHeader', ['$timeout', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function ($scope, $elem, $attrs, $ctrl) {
+                // wait for content to load into table
+                $scope.$watch(function () { return $elem.find("tbody").is(':visible'); },
+                    function (newValue, oldValue) {
+                        if (newValue === true) {
+                            // reset display styles so column widths are correct when measured below
+                            $elem.find('thead, tbody').css('display', '');
+
+                            // wrap in $timeout to give table a chance to finish rendering
+                            $timeout(function () {
+                                // set widths of columns
+                                $elem.find('th').each(function (i, thElem) {
+                                    thElem = $(thElem);
+                                    var tdElems = $elem.find('tbody tr:first td:nth-child(' + (i + 1) + ')');
+
+                                    var columnWidth = tdElems.width();
+                                    thElem.width(columnWidth);
+                                    tdElems.width(columnWidth);
+                                });
+
+                                // set css styles on thead and tbody
+                                $elem.find('thead').css({
+                                    'display': 'block',
+                                });
+
+                                $elem.find('tbody').css({
+                                    'display': 'block',
+                                    'height': '400px',
+                                    'overflow': 'auto'
+                                });
+
+                                // reduce width of last column by width of scrollbar
+                                var scrollBarWidth = $elem.find('thead').width() - $elem.find('tbody')[0].clientWidth;
+                                if (scrollBarWidth > 0) {
+                                    // for some reason trimming the width by 2px lines everything up better
+                                    scrollBarWidth -= 2;
+                                    $elem.find('tbody tr:first td:last-child').each(function (i, elem) {
+                                        $(elem).width($(elem).width() - scrollBarWidth);
+                                    });
+                                }
+                            });
+                        }
+                    });
+            }
+        };
+    }]);
+})();
